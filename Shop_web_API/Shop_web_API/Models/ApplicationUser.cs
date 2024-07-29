fb468f154e329f4cfddf20cefba9e123be65e136
HussienALiAmean <88890952+HussienALiAmean@users.noreply.github.com>
2024-07-29 21:44:25 +0300

    Delete Shop_web_API directory

--- Shop_web_API/Shop_web_API/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Shop_web_API.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- Shop_web_API/Shop_web_API/Models/DTOs/ForgotPasswordDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shop_web_API.Models.DTOs
-{
-    public class ForgotPasswordDTO
-    {
-            public string Email { get; set; }
-    }
-}
